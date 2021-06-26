10edcc4489754aa85623da599a2ee0b5bbead420
doberoi10 <oberoideepu1@gmail.com>
2021-06-26 23:55:27 +0530

    updations

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import covid from './covid.gif';
 import "./Home.css";
 
 const Home = () => {
   return (
     <div class="main" id="home">
-      Home
+      <div className="container">
+        <div className="setting-home">
+          <div className="col-sm-6 written-content">
+            <h1 className="home-head">Let's defeat <b className="covid">COVID-19</b></h1>
+            <p className="home-content">
+              If you plan on customizing the Bootstrap Sass files, or don't want
+              to use a CDN for the stylesheet, it may be helpful to install
+              vanilla Bootstrap as well.
+            </p>
+          </div>
+          <div className="col-sm-6 video">
+          <img className="video-responsive" src={covid} alt="Covid ball" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Screens/News/Info.js
@@ -0,0 +1,12 @@
+// export const News = [
+//     {
+//       S_no: "1",
+//       Pdt_imag:
+//         "https://img.tatacliq.com/images/i6/200Wx300H/MP000000004256499_437Wx649H_20200802131809.jpeg",
+//       Pdt_name: "pTron HBE5 Raptor in-Ear Stereo Sound Wired Headphones with Mic",
+//       Pdt_Price: "₹1,999/-",
+//       Pdt_brand: "JBL",
+//       Pdt_rate: "4.3/5",
+//       Pdt_description:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+//     },
